fa5ed154e12e3661a801018fec6bd724799be0fc
juliShmi <juliashmitko@gmail.com>
2026-02-17 21:18:13 +0200

    initial commit

---
+++ src/main/java/com/example/kafka_performance_lab/KafkaPerformanceLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_performance_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPerformanceLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPerformanceLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafka_performance_lab/KafkaPerformanceLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_performance_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPerformanceLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
