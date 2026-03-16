2e29ceec1e7e98a569cbeaf8e5bea37f707b8dd5
juliShmi <juliashmitko@gmail.com>
2026-03-16 23:03:02 +0200

    revert project to the working state

--- src/main/java/com/example/kafka_performance_lab/db/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kafka_performance_lab.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {}
-
--- src/main/java/com/example/kafka_performance_lab/db/OutboxStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.kafka_performance_lab.db;
-
-public enum OutboxStatus {
-    PENDING,
-    IN_FLIGHT,
-    SENT
-}
